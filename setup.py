b3e083771aee9d6b9c0509ac149d50fdb5ffb9bd
Dmitry ILiyushko <79217526217@ya.ru>
2022-10-04 18:44:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0.0',
     packages=['tests'],
     url='',
-    license='GNU GPL v3',
+    license='MIT',
     author='lightclove',
     author_email='lightclove@internet.ru',
     description='System\'s monitoring management agent of the managed modular intellectual software defined router'
